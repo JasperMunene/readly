272705789c353c66d8567178f6fbfb27f51699f1
JasperMunene <jaspermunene2022@gmail.com>
2024-10-18 19:06:24 +0300

    Changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // FETCH BOOKS
 function fetchBooks() {
-  fetch('http://localhost:3000/books')
+  fetch('https://readly-git-main-jaspers-projects-9888bea6.vercel.app/books')
     .then(response => response.json())
     .then(books => {
       const bookGrid = document.getElementById('book-grid');
